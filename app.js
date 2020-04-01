522fe44bb22696f813beb33c1980658ecbe54a67
davidorjuela <davidorjuel@gmail.com>
2020-04-01 13:42:19 -0500

    nueva version d este ejercici V5

--- app.js
+++ app.js
@@ -30,28 +30,32 @@ app.get('/', (req, res) => {
     else{
         var visitor = new Visitor({ name: 'Anónimo', count:1 });
         visitor.save(function(err){
-            Visitor.find({},(err,visitors)=>{
-                if(visitors){
-                    var html=`<table>
-                    <thead><tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Visits</th>
-                    </tr></thead>`;
-                    visitors.forEach(visitor => {
-                        html+=`
-                        <tr>
-                            <td>${visitor._id}</td>
-                            <td>${visitor.name}</td>
-                            <td>${visitor.count}</td>
-                        </tr>`;
-                    });
-                    html+=`</body></table>`;
-                    res.send(html);
-                }
-            });
+            res.send('<script>alert("Anónimo registrado")</script>'); 
         });
     }
+
+    function vista(){
+        Visitor.find({},(err,visitors)=>{
+            if(visitors){
+                var html=`<table>
+                <thead><tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Visits</th>
+                </tr></thead>`;
+                visitors.forEach(visitor => {
+                    html+=`
+                    <tr>
+                        <td>${visitor._id}</td>
+                        <td>${visitor.name}</td>
+                        <td>${visitor.count}</td>
+                    </tr>`;
+                });
+                html+=`</body></table>`;
+                res.send(html);
+            }
+        });
+    }  
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
